4d622f3a6b6bf84dba55c8be02346e643c8f8d92
Kouloumis <antkoul71@gmail.com>
2018-09-25 18:50:37 +0200

    MoveController

---
+++ MoveController.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveController : MonoBehaviour {
+
+	[SerializeField]
+	[Range(0, 30)]
+	float Speed = 30.0f;
+
+	Animator animator;
+	SpriteRenderer spriteRenderer;
+
+
+	// Use this for initialization
+	void Start () {
+		transform.position = new Vector3(0, 0, 0);
+
+		// get Access to Animator component
+		animator = GetComponent<Animator>();
+		spriteRenderer = GetComponent<SpriteRenderer>();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 currentPosition = transform.position;
+		// currentPosition.x += Input.GetAxis("Horizontal") * Speed * Time.deltaTime;
+		// currentPosition.y += Input.GetAxis("Vertical") * Speed * Time.deltaTime;
+		// transform.position = currentPosition;
+
+		// set value to characterState parameter based on input
+
+		// animation
+		if (Input.GetAxis("Horizontal") == 0){
+
+			// animator.SetInteger("characterState", 0);
+
+			if (Input.GetAxis("Vertical") == 0){
+				animator.SetInteger("characterState", 0);
+			}
+			else{
+				animator.SetInteger("characterState", 2);
+			}
+		}
+		else{
+			animator.SetInteger("characterState", 1);
+		}
+
+		if (Input.GetAxis("Horizontal") > 0){
+			spriteRenderer.flipX = false;
+		}
+		else if(Input.GetAxis("Horizontal") < 0){
+			spriteRenderer.flipX = true;
+		}
+		
+		if (Input.GetAxis("Vertical") > 0){
+			spriteRenderer.flipY = false;
+		}
+		else if(Input.GetAxis("Vertical") < 0){
+			spriteRenderer.flipY = true;
+		}
+
+		// movement
+		transform.Translate(Input.GetAxis("Horizontal") * Speed * Time.deltaTime, Input.GetAxis("Vertical") * Speed * Time.deltaTime, 0);
+	}
+}
